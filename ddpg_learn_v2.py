cf0f214de77dfb3dd65c49dc789e9a2fbed7c4b3
jrlodge <jackryanlodge@gmail.com>
2022-04-27 00:22:54 +1000

    Small changes

--- ddpg_learn_v2.py
+++
@@ -1,4 +0,0 @@
-from the_beer_game_environment import BeerGameEnv
-
-env = BeerGameEnv()
-print(env.action_space.shape[-1])
--- the_beer_game_ddpg_learn.py
+++ the_beer_game_ddpg_learn.py
@@ -6,6 +6,8 @@ import time
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
+TIMESTEPS = 1000
+
 writer = SummaryWriter('the_beer_game/ddpg')
 
 models_dir = f"models/{int(time.time())}/"
@@ -24,11 +26,11 @@ env.reset()
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = DDPG('MlpPolicy', env, verbose=1, action_noise=action_noise, tensorboard_log=logdir)
+model = DDPG('MlpPolicy', env=env, action_noise=action_noise, tensorboard_log=logdir, verbose=1)
+model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, log_interval=10, tb_log_name=f"DDPG_LOG")
 
-TIMESTEPS = 10000
-iters = 0
+'''iters = 0
 while True:
 	iters += 1
-	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"DDPG")
-	model.save(f"{models_dir}/{TIMESTEPS*iters}")
+	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, log_interval=10, tb_log_name=f"DDPG")
+	model.save(f"{models_dir}/{TIMESTEPS*iters}")'''
